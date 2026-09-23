c62f0ebef181448583df3a73ca6bef965df62020
FeliciaBo <theskylla@gmail.com>
2026-09-23 16:59:56 +0200

    add PhoneShop class

---
+++ src/main/java/di/example/part2/PhoneShop.java
@@ -0,0 +1,19 @@
+package di.example.part2;
+
+/* PhoneStore = Top level class.
+PhoneStore needs a phone, and
+a Phone needs an OperatingSystem.*/
+
+public class PhoneShop {
+
+    private final Phone phone;
+
+    public PhoneShop(Phone phone) {
+        this.phone = phone;
+    }
+
+    public void sellPhone() {
+        IO.println("Selling phone...");
+        phone.turnOn();
+    }
+}
