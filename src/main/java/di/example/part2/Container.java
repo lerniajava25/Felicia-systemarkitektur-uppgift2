1549864d359400d6d971fa19bf9805b0479428bb
FeliciaBo <theskylla@gmail.com>
2026-09-24 10:59:51 +0200

    fix container code according to coderabbit feedback

--- src/main/java/di/example/part2/Container.java
+++ src/main/java/di/example/part2/Container.java
@@ -2,11 +2,15 @@ package di.example.part2;
 
 import java.lang.reflect.Constructor;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Container {
 
     private final Map<Class<?>, Class<?>> bindings = new HashMap<>();
+
+    private final Set<Class<?>> resolutionPath = new HashSet<>();
 
     public <T> void bind(Class<T> type, Class<? extends T> implementation) {
         bindings.put(type, implementation);
@@ -14,24 +18,36 @@ public class Container {
 
     public <T> T resolve(Class<T> type) {
         Class<?> implClass = bindings.getOrDefault(type, type);
-        Constructor<?>[] constructors = implClass.getConstructors();
 
-        if (constructors.length == 0) {
-            throw new IllegalStateException(
-                    "No public constructor available");
+        if (!resolutionPath.add(implClass)) {
+            throw new IllegalStateException("Circular dependency detected");
         }
-        Constructor<?> constructor = constructors[0];
-
-        Class<?>[] paramTypes = constructor.getParameterTypes();
-        Object[] dependencies = new Object[paramTypes.length];
-        for (int i = 0; i < paramTypes.length; i++) {
-            dependencies[i] = resolve(paramTypes[i]);
-        }
-
         try {
-            return type.cast(constructor.newInstance(dependencies));
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException("Could not instantiate " + implClass, e);
+            Constructor<?>[] constructors = implClass.getConstructors();
+
+            if (constructors.length == 0) {
+                throw new IllegalStateException(
+                        "No public constructor available for " + implClass.getName());
+            }
+
+            if (constructors.length > 1) {
+                throw new IllegalStateException
+                        ("Exactly one public constructor expected, found: " + implClass.getName());
+            }
+
+            Constructor<?> constructor = constructors[0];
+
+            Class<?>[] paramTypes = constructor.getParameterTypes();
+            Object[] dependencies = new Object[paramTypes.length];
+            for (int i = 0; i < paramTypes.length; i++) {
+                dependencies[i] = resolve(paramTypes[i]);
+            }
+                return type.cast(constructor.newInstance(dependencies));
+            } catch (ReflectiveOperationException e) {
+                throw new RuntimeException("Could not instantiate " + implClass, e);
+            } finally {
+                resolutionPath.remove(implClass);
+            }
         }
     }
-}
+
